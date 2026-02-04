1858595823adbd375e699c086564fe3dbf373353
MaxDeMan1 <maxburna4@gmail.com>
2026-02-04 19:01:20 +1100

    Experiment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tasks
 from tkinter import *
 from tkinter import ttk
 
@@ -5,5 +6,16 @@ root = Tk()
 root.title("todoapp")
 main_frame = ttk.Frame(root, padding=(3, 3, 12, 12))
 main_frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+ttk.Label(main_frame, text="Task Name:").grid(column=1, row=1, sticky=(W))
+task_name = StringVar()
+task_name_entry = ttk.Entry(main_frame, width=35, textvariable=task_name)
+task_name_entry.grid(column=2, row=1, sticky=(W, E))
+
+task = tasks.Task(task_name)
+add_task_button = ttk.Button(main_frame, text="Add Task", command=tasks.add_task(task))
+add_task_button.grid(column=2, row=2, sticky=(W, E))
+
+ttk.Label(main_frame, text=str(tasks.tasks)).grid(column=3, row=3, sticky=(W, E))
 
 root.mainloop()
