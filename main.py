7d494778e88f226819c2963d1127cdab25cdb828
MaxDeMan1 <maxburna4@gmail.com>
2026-02-09 21:48:15 +1100

    WIP of interactive CLI functionality

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import sys
 import tasks
-# import json
-# a = tasks.Task({"name": "Task 1", "priority": "normal", "deadline": None, "description": None})
-a = tasks.create_task("Task 1")
-print(a)
+import json
+import typing
 
-# tasks.add_task(tasks.Task("Task 1"))
-# with open("tasks.json", "w") as file:
-#     json.dump(tasks.tasks, file, default=lambda x: x.__dict__)
 
-# tasks.clear_tasks()
-# print(tasks.tasks)
+def main():
+    # Loads the saved tasks
+    with open("tasks.json", "r") as file:
+        tasks.tasks = json.load(file)
 
-# # tasks.tasks = json.loads(js)
-# print(tasks.tasks)
+    while True:
+        # TODO: Wrap grapping input in try/except
+        user_input: str = input("Enter command (? for help): ")
+        split_input: typing.List[str] = user_input.split()
+        match split_input[0]:
+            case "?":
+                print(
+                    "?\t\t\tHelp (this screen)"
+                    "\nadd <name> <priority>\tAdds task"
+                    "\nremove <name>\t\tRemoves the specified task based on name"
+                    "\nclear\t\t\tRemoves all tasks"
+                    "\nshow <name>\t\tShows info about the task"
+                    "\nExit with ctrl+c"
+                )
+            case "add": # TODO
+                pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:  # ctrl+c
+        # Saves the curent tasks
+        with open("tasks.json", "w") as file:
+            json.dump(tasks.tasks, file, default=lambda x: x.__dict__)
+        print()  # Print newline
+        exit(0)
